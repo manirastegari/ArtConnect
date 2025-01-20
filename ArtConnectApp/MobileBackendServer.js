e551d5700bbeeb3c97e85f8feac2b4d1fffc4288
Mani Rastegari <mani.rastegari@gmail.com>
2025-01-20 17:58:33 -0500

    DefaultUserImageOnArtsTab

--- ArtConnectApp/MobileBackendServer.js
+++ ArtConnectApp/MobileBackendServer.js
@@ -316,4 +316,17 @@ router.post('/logout', (req, res) => {
   res.status(200).json({ message: 'Logout successful' });
 });
 
+// Add this route to your userRoutes
+router.get('/details/:id', async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id).select('fullname email type');
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
 module.exports = router;
