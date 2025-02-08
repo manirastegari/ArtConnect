ecedd09e0b3c6a5ad976714afee89ba93812c1f4
Mani Rastegari <mani.rastegari@gmail.com>
2025-02-08 10:31:32 -0500

    ci-cdAddedForGithubActionV7RunnerLocalV3SetupforAppcenterDistributeReadyV4

--- ArtConnectApp/App.js
+++ ArtConnectApp/App.js
@@ -10,12 +10,14 @@ import PostArtScreen from './screens/PostArtScreen';
 import PostEventScreen from './screens/PostEventScreen';
 
 // Import App Center modules
-import AppCenter from 'appcenter';
-import AppCenterAnalytics from 'appcenter-analytics';
-import AppCenterCrashes from 'appcenter-crashes';
+// import AppCenter from 'appcenter';
+// import AppCenterAnalytics from 'appcenter-analytics';
+// import AppCenterCrashes from 'appcenter-crashes';
 
-// Initialize App Center
-AppCenter.start("568d8f1e-876f-465f-b6d5-1de31a00b810", AppCenterAnalytics, AppCenterCrashes);
+// console.log('AppCenter:', AppCenter);
+
+// // Initialize App Center
+// AppCenter.start("568d8f1e-876f-465f-b6d5-1de31a00b810", AppCenterAnalytics, AppCenterCrashes);
 
 const Stack = createStackNavigator();
 
@@ -34,5 +36,5 @@ export default function App() {
       </NavigationContainer>
     </AuthProvider>
   );
-  
+
 }
